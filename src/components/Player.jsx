6133a78ab70737d5ca788954dd603b94e0318cd1
Anees <aneesshafique@gmail.com>
2024-07-01 17:47:42 +0500

    Final Countdown React completed

---
+++ src/components/Player.jsx
@@ -0,0 +1,19 @@
+import { useState, useRef } from "react";
+
+export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState();
+  const playerName = useRef();
+
+  function handleClick(event) {
+    setEnteredPlayerName(playerName.current.value);
+  }
+  return (
+    <section id="player">
+      <h2>Welcome {enteredPlayerName ?? enteredPlayerName}</h2>
+      <p>
+        <input ref={playerName} type="text"/>
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
